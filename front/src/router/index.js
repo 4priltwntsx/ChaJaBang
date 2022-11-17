e0b2e12a34c84ce6dcc459199e64a77cae42febc
김지희 <cjg05034@naver.com>
2022-11-17 16:46:44 +0900

    add: user vuex

--- front/src/router/index.js
+++ front/src/router/index.js
@@ -8,10 +8,36 @@ import BoardList from "../components/board/BoardList.vue"
 import BoardNotice from "../components/board/BoardNotice.vue"
 import BoardDetail from "../components/board/BoardDetail.vue"
 import UserLogin from "@/components/user/UserLogin"
-import UserView from "@/views/UserView"
+import UserMyPage from "@/components/user/UserMyPage"
+import UserRegister from "@/components/user/UserRegister"
+
 import BoardModify from "@/components/board/BoardModify.vue"
 
+import store from "@/store";
+
 Vue.use(VueRouter);
+
+// https://v3.router.vuejs.org/kr/guide/advanced/navigation-guards.html
+const onlyAuthUser = async (to, from, next) => {
+  const checkUserInfo = store.getters["memberStore/checkUserInfo"];
+  const checkToken = store.getters["memberStore/checkToken"];
+  let token = sessionStorage.getItem("access-token");
+  console.log("로그인 처리 전", checkUserInfo, token);
+
+  if (checkUserInfo != null && token) {
+    console.log("토큰 유효성 체크하러 가자!!!!");
+    await store.dispatch("memberStore/getUserInfo", token);
+  }
+  if (!checkToken || checkUserInfo === null) {
+    alert("로그인이 필요한 페이지입니다..");
+    // next({ name: "login" });
+    router.push({ name: "login" });
+  } else {
+    console.log("로그인 했다!!!!!!!!!!!!!.");
+    next();
+  }
+};
+
 
 const routes = [
   {
@@ -58,15 +84,25 @@ const routes = [
   {
     path: "/user",
     name: "user",
-    component:UserView,
+    component: () => import("@/views/UserView"),
     children: [
       {
-        path: "/login",
-        name: "Login",
+        path: "join",
+        name: "join",
+        component: UserRegister,
+      },
+      {
+        path: "login",
+        name: "login",
         component: UserLogin,
       },
-
-    ]
+      {
+        path: "mypage",
+        name: "mypage",
+        beforeEnter: onlyAuthUser,
+        component: UserMyPage,
+      },
+    ],
   },
 ];
 
