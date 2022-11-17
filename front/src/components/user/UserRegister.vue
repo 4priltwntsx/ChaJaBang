b465ddb2639d6ee04a10cb6347ad44f61e22ef78
김지희 <cjg05034@naver.com>
2022-11-17 17:30:44 +0900

    add: register

---
+++ front/src/components/user/UserRegister.vue
@@ -0,0 +1,73 @@
+<template>
+  <div>
+    <h1> Sign Up</h1>
+    <v-form ref="form" v-model="valid" lazy-validation>
+
+      <v-text-field v-model="id" :counter="15" :rules="idRules" label="Name" required></v-text-field>
+
+      <v-text-field v-model="password" :append-icon="show1 ? 'mdi-eye' : 'mdi-eye-off'"
+        :rules="[rules.required, rules.min]" :type="show1 ? 'text' : 'password'" name="input-10-1"
+        label="Normal with hint text" hint="At least 8 characters" counter @click:append="show1 = !show1">
+      </v-text-field>
+
+      <v-text-field v-model="name" label="Name" required></v-text-field>
+
+      <v-text-field v-model="email" :rules="emailRules" label="E-mail" required></v-text-field>
+
+      <v-btn :disabled="!valid" color="success" class="mr-4" @click="validate">
+        Regist
+      </v-btn>
+
+      <v-btn color="error" class="mr-4" @click="reset">
+        Cancel
+      </v-btn>
+
+      <v-btn color="warning" @click="resetValidation">
+        to login
+      </v-btn>
+    </v-form>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "UserRegister",
+    data() {
+      return {
+        valid: true,
+        id: '',
+        idRules: [
+          v => !!v || 'id is required',
+          v => (v && v.length <= 15) || 'id must be less than 15 characters',
+        ],
+
+        show1: false,
+        password: '',
+        rules: {
+          required: value => !!value || 'Required.',
+          min: v => v.length >= 8 || 'Min 8 characters',
+          emailMatch: () => (`The email and password you entered don't match`),
+        },
+
+        name:'',
+
+        email: '',
+        emailRules: [
+          v => !!v || 'E-mail is required',
+          v => /.+@.+\..+/.test(v) || 'E-mail must be valid',
+        ],
+      }
+    },
+
+    computed:{},
+    methods:{
+      
+    }
+
+
+  }
+</script>
+
+<style>
+
+</style>
