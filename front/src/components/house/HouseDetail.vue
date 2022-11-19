f60541401fd95cb97b9d723a266af241482696a3
김승희 <kanada11416@gmail.com>
2022-11-19 15:13:18 +0900

    add : traffic information

--- front/src/components/house/HouseDetail.vue
+++ front/src/components/house/HouseDetail.vue
@@ -1,64 +1,63 @@
 <template>
-<div>
-  <h3>{{house.apartmentName}}</h3>
-  <h4>{{house.buildYear}} 준공 </h4>      
-  <v-btn @click="move2Table"
-        class="ma-2"
-        color="indigo darken-2"
-        dark
-      >
-        <v-icon
-          dark
-          left
-        >
-          mdi-arrow-left
-        </v-icon>Back
-      </v-btn>
-  <v-simple-table fixed-header max-width="580" height="590">
-    <template v-slot:default>
-      <thead>
-        <tr>
-          <th class="text-left">거래번호</th>
-          <th class="text-left">거래일자</th>
-          <th class="text-left">층수</th>
-          <th class="text-left">거래금액</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr v-for="item in deals" :key="item.no">
-          <td>{{ item.no }}</td>
-          <td>{{ item.dealYear}}년 {{item.dealMonth}}월 {{item.dealDay}}일</td>
-          <td>{{ item.floor }}층</td>
-          <td>{{ item.dealAmount }}원</td>
-        </tr>
-      </tbody>
-    </template>
-  </v-simple-table>
+  <div>
+    <h3>{{ house.apartmentName }}</h3>
+    <h4>{{ house.buildYear }} 준공</h4>
+    <v-btn @click="move2Table" class="ma-2" color="indigo darken-2" dark>
+      <v-icon dark left> mdi-arrow-left </v-icon>Back
+    </v-btn>
+    <v-data-table
+      :headers="headers"
+      :items="deals"
+      :search="search"
+      :custom-filter="floorFilter"
+    ></v-data-table>
   </div>
 </template>
 
 <script>
-import {mapState} from "vuex";
+import { mapState } from "vuex";
 const houseStore = "houseStore";
 
 export default {
   name: "HouseDetail",
-  data(){
-    return{
+  data() {
+    return {
       aptCode: null,
+      search: "",
+      headers: [
+        {
+          text: "거래번호",
+          align: "start",
+          sortable: "false",
+          value: "no",
+        },
+        {
+          text: "층수(층)",
+          value: "floor",
+          filter: (value) => {
+            return value + "floor";
+          },
+        },
+        {
+          text: "면적",
+          value: "area",
+        },
+        {
+          text: "거래일",
+          value: "dealYear",
+        },
+      ],
     };
   },
-  computed:{
+  computed: {
     ...mapState(houseStore, ["houses", "points", "house", "deals"]),
-    
   },
-  methods:{
-    move2Table(){
-      this.$router.push({name:"houseTable"})
-    }
+  methods: {
+    move2Table() {
+      this.$router.push({ name: "houseTable" });
+    },
   },
-}
+};
 </script>
 
-<style>
-</style>
+<style></style>
